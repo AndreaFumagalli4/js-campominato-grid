ece90a3fa5f5cbc69e1b6212f27231347dcb6d26
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-30 17:37:18 +0100

    fix function getNewSquare

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 // Funzione per creare l'elemento
 
-function getNewSqaure () {
+function getNewSqaure (index) {
     const newSquare = document.createElement('div');
     newSquare.classList.add('square', 'd-flex');
+    newSquare.innerHTML = `<span class="m-auto"> ${index} </span>`;
+        newSquare.addEventListener('click', function(){
+            console.log('Hai cliccato la casella ' + index);
+            newSquare.classList.toggle('active');
+        })
     return newSquare;
 }
 
@@ -12,13 +17,10 @@ button.addEventListener('click', function(){
 
     const gridContainer = document.querySelector('div.grid');
 
+    gridContainer.innerHTML = '';
+
     for (let i = 1; i <= 100; i++) {
-        const newSquare = getNewSqaure();
-        newSquare.innerHTML = `<span class="m-auto"> ${i} </span>`;
-        newSquare.addEventListener('click', function(){
-            console.log('Hai cliccato la casella ' + i);
-            newSquare.classList.toggle('active');
-        })
+        const newSquare = getNewSqaure(i);
         gridContainer.appendChild(newSquare);
     }
 })
