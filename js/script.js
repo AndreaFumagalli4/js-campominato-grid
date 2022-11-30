e7d2fd6bdcecdbb85fa5e3395bd67180c86b49b7
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-30 16:41:26 +0100

    add addEventListener

--- js/script.js
+++ js/script.js
@@ -2,20 +2,27 @@
  *  Creare i quadrati che riempino il field (ATT!! CREARLI NELL' ADDEVENTLISTENER perchè bisogna crearli quando l'utente clicca su play!!)
  */
 
-// Creazione div
-const gridContainer = document.querySelector('div.grid');
+// Mostro il riquadro di gioco quando l'utente clicca play
 
-// create 100 divs
-for (let i = 1; i <= 100; i++) {
-    const newSquare = document.createElement('div');
-    // Aggiunta classe ai div creati
-    newSquare.classList.add('square', 'd-flex');
-    // Aggiunta numeri interi square
-    newSquare.innerHTML = `<span class="m-auto"> ${i} </span>`;
-    // Add click
-    newSquare.addEventListener('click', function(){
-        console.log('Hai cliccato la casella ' + i);
-        newSquare.classList.toggle('active');
-    })
-    gridContainer.appendChild(newSquare);
-}
+const button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+// Creazione div
+    const gridContainer = document.querySelector('div.grid');
+
+    // create 100 divs
+    for (let i = 1; i <= 100; i++) {
+        const newSquare = document.createElement('div');
+        // Aggiunta classe ai div creati
+        newSquare.classList.add('square', 'd-flex');
+        // Aggiunta numeri interi square
+        newSquare.innerHTML = `<span class="m-auto"> ${i} </span>`;
+        // Add click
+        newSquare.addEventListener('click', function(){
+            console.log('Hai cliccato la casella ' + i);
+            newSquare.classList.toggle('active');
+        })
+        gridContainer.appendChild(newSquare);
+    }
+})
+
