fd340d0cd5a8c1ba2b6cc918f539db1d170d8e5b
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-30 15:45:28 +0100

    made a square

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
  *  Creare i quadrati che riempino il field (ATT!! CREARLI NELL' ADDEVENTLISTENER perchè bisogna crearli quando l'utente clicca su play!!)
  */
 
+// Contatore da 1 a 100
 for (let i = 1; i <= 100; i++) {
     console.log(i);
 }
+
+// Creazione div
+const gridContainer = document.querySelector('div.grid');
+
+const newSquare = document.createElement('div');
+
+gridContainer.appendChild(newSquare);
 
