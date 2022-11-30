7b5356273d272c2b8285a97ab3217f24dee810e2
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-30 15:55:23 +0100

    add square class to div

--- js/script.js
+++ js/script.js
@@ -2,20 +2,13 @@
  *  Creare i quadrati che riempino il field (ATT!! CREARLI NELL' ADDEVENTLISTENER perchè bisogna crearli quando l'utente clicca su play!!)
  */
 
-// Contatore da 1 a 100
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
-
 // Creazione div
 const gridContainer = document.querySelector('div.grid');
-
-// const newSquare = document.createElement('div');
-
-// gridContainer.appendChild(newSquare);
 
 // create 100 divs
 for (let i = 1; i <= 100; i++) {
     const newSquare = document.createElement('div');
+    // Aggiunta classe ai div creati
+    newSquare.classList.add('square');
     gridContainer.appendChild(newSquare);
 }
