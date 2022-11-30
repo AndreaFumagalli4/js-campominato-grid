57ea63dffa5f75cc9ef7958f2ac7fe8eb5d07e4a
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-30 15:41:39 +0100

    add for cycle

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**
+ *  Creare i quadrati che riempino il field (ATT!! CREARLI NELL' ADDEVENTLISTENER perchè bisogna crearli quando l'utente clicca su play!!)
+ */
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
