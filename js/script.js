ce3e08e0ce4a6fff851e82ec8283a9125d8de814
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-30 17:07:19 +0100

    add function to create square

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
-/**
- *  Creare i quadrati che riempino il field (ATT!! CREARLI NELL' ADDEVENTLISTENER perchè bisogna crearli quando l'utente clicca su play!!)
- */
+// Funzione per creare l'elemento
 
-// Mostro il riquadro di gioco quando l'utente clicca play
+function getNewSqaure () {
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square', 'd-flex');
+    return newSquare;
+}
 
 const button = document.getElementById('button');
 
 button.addEventListener('click', function(){
-// Creazione div
+
     const gridContainer = document.querySelector('div.grid');
 
-    // create 100 divs
     for (let i = 1; i <= 100; i++) {
-        const newSquare = document.createElement('div');
-        // Aggiunta classe ai div creati
-        newSquare.classList.add('square', 'd-flex');
-        // Aggiunta numeri interi square
+        const newSquare = getNewSqaure();
         newSquare.innerHTML = `<span class="m-auto"> ${i} </span>`;
-        // Add click
         newSquare.addEventListener('click', function(){
             console.log('Hai cliccato la casella ' + i);
             newSquare.classList.toggle('active');
